dd2dbd854bf08fb3cd6f4e6cb2533da4403d8aa7
mohayonao <mohayonao@gmail.com>
2012-12-18 16:26:15 +0900

    add T("map")

---
+++ src/objects/map.js
@@ -0,0 +1,165 @@
+(function() {
+    "use strict";
+    
+    function Map(_args) {
+        timbre.Object.call(this, _args);
+        
+        this._.inMin  = 0;
+        this._.inMax  = 1;
+        this._.outMin = 0;
+        this._.outMax = 1;
+        
+        this.once("init", oninit);
+        
+        timbre.fn.fixKR(this);
+    }
+    timbre.fn.extend(Map, timbre.Object);
+    
+    var oninit = function() {
+        if (!this._.warp) {
+            this.warp = "linlin";
+        }
+    };
+    
+    var $ = Map.prototype;
+    
+    Object.defineProperties($, {
+        inMin: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    this._.inMin = value;
+                }
+            },
+            get: function() {
+                return this._.inMin;
+            }
+        },
+        inMax: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    this._.inMax = value;
+                }
+            },
+            get: function() {
+                return this._.inMax;
+            }
+        },
+        outMin: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    this._.outMin = value;
+                }
+            },
+            get: function() {
+                return this._.outMin;
+            }
+        },
+        outMax: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    this._.outMax = value;
+                }
+            },
+            get: function() {
+                return this._.outMax;
+            }
+        },
+        warp: {
+            set: function(value) {
+                if (typeof value === "string") {
+                    var f = WarpFunctions[value];
+                    if (f) {
+                        this._.warp = f;
+                        this._.warpName = value;
+                    }
+                }
+            },
+            get: function() {
+                return this._.warpName;
+            }
+        }
+    });
+    
+    $.seq = function(seq_id) {
+        var _ = this._;
+        var cell = this.cell;
+        
+        if (this.seq_id !== seq_id) {
+            this.seq_id = seq_id;
+            
+            var inputs  = this.inputs;
+            var i, imax = inputs.length;
+            var j, jmax = cell.length;
+            var x;
+            
+            x = 0;
+            for (i = 0; i < imax; ++i) {
+                x += inputs[i].seq(seq_id)[0];
+            }
+
+            var inMin  = _.inMin, inMax   = _.inMax;
+            var outMin = _.outMin, outMax = _.outMax;
+            var warp   = _.warp;
+            
+            x = warp(x, inMin, inMax, outMin, outMax) * _.mul + _.add;
+            
+            for (j = jmax; j--; ) {
+                cell[j] = x;
+            }
+        }
+        
+        return cell;
+    };
+    
+    var WarpFunctions = {
+        linlin: function(x, inMin, inMax, outMin, outMax) {
+            if (x < inMin) {
+                return outMin;
+            } else if (x > inMax) {
+                return outMax;
+            }
+            if (inMax === inMin) {
+                return outMin;
+            }
+            return (x-inMin) / (inMax-inMin) * (outMax-outMin) + outMin;
+        },
+        linexp: function(x, inMin, inMax, outMin, outMax) {
+            if (x < inMin) {
+                return outMin;
+            } else if (x > inMax) {
+                return outMax;
+            }
+            if (outMin === 0) {
+                return 0;
+            }
+            if (inMax === inMin) {
+                return outMax;
+            }
+            return Math.pow(outMax/outMin, (x-inMin)/(inMax-inMin)) * outMin;
+        },
+        explin: function(x, inMin, inMax, outMin, outMax) {
+            if (x < inMin) {
+                return outMin;
+            } else if (x > inMax) {
+                return outMax;
+            }
+            if (inMin === 0) {
+                return outMax;
+            }
+            return Math.log(x/inMin) / Math.log(inMax/inMin) * (outMax-outMin) + outMin;
+        },
+        expexp: function(x, inMin, inMax, outMin, outMax) {
+            if (x < inMin) {
+                return outMin;
+            } else if (x > inMax) {
+                return outMax;
+            }
+            if (inMin === 0 || outMin === 0) {
+                return 0;
+            }
+            return Math.pow(outMax/outMin, Math.log(x/inMin) / Math.log(inMax/inMin)) * outMin;
+        }
+    };
+    
+    timbre.fn.register("map", Map);
+})();
