b7dc7fb3987cdf34c7581402ba53eac7d6ef63f3
mohayonao <mohayonao@gmail.com>
2012-12-20 07:21:38 +0900

    add T("ifft") draft

---
+++ docs.md/ja/_ifft.md
@@ -0,0 +1,10 @@
+T("ifft")
+=========
+
+```timbre
+var osc = T("saw");
+
+var fft  = T("fft").listen(osc);
+var real = T("sin", {freq:1}, fft.real);
+var ifft = T("ifft", {real:real, imag:fft.imag}).play();
+```
---
+++ src/objects/_ifft.js
@@ -0,0 +1,70 @@
+(function() {
+    "use strict";
+    
+    var FFT = timbre.utils.FFT;
+    
+    function IFFT(_args) {
+        timbre.Object.call(this, _args);
+        timbre.fn.fixAR(this);
+        
+        this._.fft = new FFT(timbre.cellsize * 2);
+        this._.fftCell    = new Float32Array(this._.fft.length);
+        this._.realBuffer = new Float32Array(this._.fft.length);
+        this._.imagBuffer = new Float32Array(this._.fft.length);
+    }
+    timbre.fn.extend(IFFT, timbre.Object);
+    
+    var $ = IFFT.prototype;
+    
+    Object.defineProperties($, {
+        real: {
+            set: function(value) {
+                this._.real = timbre(value);
+            },
+            get: function() {
+                return this._.real;
+            }
+        },
+        imag: {
+            set: function(value) {
+                this._.imag = timbre(value);
+            },
+            get: function() {
+                return this._.imag;
+            }
+        }
+    });
+    
+    $.seq = function(seq_id) {
+        var _ = this._;
+        var cell = this.cell;
+        
+        if (this.seq_id !== seq_id) {
+            this.seq_id = seq_id;
+            
+            if (_.real && _.imag) {
+                var real = _.realBuffer;
+                var imag = _.imagBuffer;
+                var _real = _.real.seq(seq_id);
+                var _imag = _.imag.seq(seq_id);
+                var i = cell.length, imax = real.length;
+                var j, jmax = cell.length;
+                var k = cell.length - 1;
+                var mul = _.mul, add = _.add;
+                
+                real.set(_real);
+                imag.set(_imag);
+                
+                cell.set(_.fft.inverse(real, imag).subarray(0, cell.length));
+                
+                for (j = jmax; j--; ) {
+                    cell[j] = cell[j] * mul + add;
+                }
+            }
+        }
+        
+        return cell;
+    };
+    
+    timbre.fn.register("ifft", IFFT);
+})();
