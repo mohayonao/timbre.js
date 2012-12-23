34ea4d2f43da8c76969a6329d4c3dfb52bbf29e8
mohayonao <mohayonao@gmail.com>
2012-12-24 08:20:07 +0900

    split files

---
+++ src/objects/spectrum.js
@@ -0,0 +1,210 @@
+(function(timbre) {
+    "use strict";
+    
+    var timevalue = timbre.utils.timevalue;
+    var FFT = timbre.utils.FFT;
+    
+    function FFTSpectrum(_args) {
+        timbre.Object.call(this, _args);
+        timbre.fn.listener(this);
+        timbre.fn.fixAR(this);
+        
+        this._.status  = 0;
+        this._.samples = 0;
+        this._.samplesIncr = 0;
+        this._.writeIndex  = 0;
+        
+        this._.plotFlush = true;
+        this._.plotRange = [0, 0.5];
+        this._.plotBarStyle = true;
+        
+        this.once("init", oninit);
+    }
+    timbre.fn.extend(FFTSpectrum, timbre.Object);
+    
+    var oninit = function() {
+        var _ = this._;
+        if (!_.fft) {
+            this.size = 512;
+        }
+        if (!_.interval) {
+            this.interval = 500;
+        }
+    };
+    
+    var $ = FFTSpectrum.prototype;
+    
+    Object.defineProperties($, {
+        size: {
+            set: function(value) {
+                var _ = this._;
+                if (!_.fft) {
+                    if (typeof value === "number") {
+                        var n = (value < 256) ? 256 : (value > 2048) ? 2048 : value;
+                        _.fft    = new FFT(n);
+                        _.buffer = new Float32Array(_.fft.length);
+                        if (_.reservedwindow) {
+                            _.fft.setWindow(_.reservedwindow);
+                            _.reservedwindow = null;
+                        }
+                        if (_.reservedinterval) {
+                            this.interval = _.reservedinterval;
+                            _.reservedinterval = null;
+                        }
+                    }
+                }
+            },
+            get: function() {
+                return this._.buffer.length;
+            }
+        },
+        window: {
+            set: function(value) {
+                this._.fft.setWindow(value);
+            },
+            get: function() {
+                return this._.fft.windowName;
+            }
+        },
+        interval: {
+            set: function(value) {
+                var _ = this._;
+                if (typeof value === "string") {
+                    value = timevalue(value);
+                }
+                if (typeof value === "number" && value > 0) {
+                    if (!_.buffer) {
+                        _.reservedinterval = value;
+                    } else {
+                        _.interval = value;
+                        _.samplesIncr = (value * 0.001 * timbre.samplerate);
+                        if (_.samplesIncr < _.buffer.length) {
+                            _.samplesIncr = _.buffer.length;
+                            _.interval = _.samplesIncr * 1000 / timbre.samplerate;
+                        }
+                    }
+                }
+            },
+            get: function() {
+                return this._.interval;
+            }
+        },
+        spectrum: {
+            get: function() {
+                return this._.fft.spectrum;
+            }
+        },
+        real: {
+            get: function() {
+                return this._.fft.real;
+            }
+        },
+        imag: {
+            get: function() {
+                return this._.fft.imag;
+            }
+        }
+    });
+    
+    $.bang = function() {
+        this._.samples    = 0;
+        this._.writeIndex = 0;
+        this._.emit("bang");
+        return this;
+    };
+    
+    $.seq = function(seq_id) {
+        var _ = this._;
+        var cell = this.cell;
+
+        if (this.seq_id !== seq_id) {
+            this.seq_id = seq_id;
+            
+            var inputs = this.inputs;
+            var i, imax = inputs.length;
+            var j, jmax = cell.length;
+            var tmp;
+            
+            for (j = jmax; j--; ) {
+                cell[j] = 0;
+            }
+            
+            for (i = 0; i < imax; ++i) {
+                tmp = inputs[i].seq(seq_id);
+                for (j = jmax; j--; ) {
+                    cell[j] += tmp[j];
+                }
+            }
+            
+            var status  = _.status;
+            var samples = _.samples;
+            var samplesIncr = _.samplesIncr;
+            var writeIndex  = _.writeIndex;
+            var buffer = _.buffer;
+            var bufferLength = buffer.length;
+            var mul = _.mul, add = _.add;
+            var emit;
+            
+            for (j = 0; j < jmax; ++j) {
+                if (samples <= 0) {
+                    if (status === 0) {
+                        status = 1;
+                        writeIndex  = 0;
+                        samples += samplesIncr;
+                    }
+                }
+                if (status === 1) {
+                    buffer[writeIndex++] = cell[j];
+                    if (bufferLength <= writeIndex) {
+                        _.fft.forward(buffer);
+                        emit = _.plotFlush = true;
+                        status = 0;
+                    }
+                }
+                cell[j] = cell[j] * mul + add;
+                --samples;
+            }
+            
+            _.samples = samples;
+            _.status  = status;
+            _.writeIndex = writeIndex;
+            
+            if (emit) {
+                this._.emit("fft");
+            }
+        }
+        return cell;
+    };
+    
+    var super_plot = timbre.Object.prototype.plot;
+    
+    $.plot = function(opts) {
+        if (this._.plotFlush) {
+            var fft = this._.fft;
+            
+            var spectrum = fft.spectrum;
+            var step     = fft.length >> 6;
+            var istep    = 1 / step;
+            var data    = new Float32Array(spectrum.length * istep);
+            var i, imax = spectrum.length;
+            var j, jmax = step;
+            
+            var v, k = 0;
+            for (i = 0; i < imax; i += step) {
+                v = 0;
+                for (j = 0; j < jmax; ++j) {
+                    v += spectrum[i + j];
+                }
+                data[k++] = v * istep;
+            }
+            
+            this._.plotData  = data;
+            this._.plotFlush = null;
+        }
+        return super_plot.call(this, opts);
+    };
+    
+    timbre.fn.register("spectrum", FFTSpectrum);
+
+})(timbre);
+
