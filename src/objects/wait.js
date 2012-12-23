98320701d4d13baba80e638aca42f48420cef1df
mohayonao <mohayonao@gmail.com>
2012-12-24 08:43:59 +0900

    T("timeout") is not deferred

--- src/objects/wait.js
+++ src/objects/wait.js
@@ -3,16 +3,20 @@
     
     var timevalue = timbre.utils.timevalue;
     
+    var TYPE_WAIT    = 0;
+    var TYPE_TIMEOUT = 1;
+    
     function Wait(_args) {
         timbre.Object.call(this, _args);
         timbre.fn.timer(this);
         timbre.fn.fixKR(this);
-        timbre.fn.deferred(this);
         
         this._.currentTime = 0;
         this._.currentTimeIncr = timbre.cellsize * 1000 / timbre.samplerate;
         
         this._.waitSamples = 0;
+        this._.samples = 0;
+        this._.isEnded = true;
         
         this.once("init", oninit);
     }
@@ -22,19 +26,28 @@
         if (!this._.time) {
             this.time = 1000;
         }
+        if (this._.originkey === "wait") {
+            this._.type = TYPE_WAIT;
+            timbre.fn.deferred(this);
+        } else {
+            this._.type = TYPE_TIMEOUT;
+        }
     };
     
     var $ = Wait.prototype;
-
+    
     Object.defineProperties($, {
         time: {
             set: function(value) {
+                var _ = this._;
                 if (typeof value === "string") {
                     value = timevalue(value);
                 }
                 if (typeof value === "number" && value >= 0) {
-                    this._.time = value;
-                    this._.waitSamples = (timbre.samplerate * (value * 0.001))|0;
+                    _.time = value;
+                    _.waitSamples = (timbre.samplerate * (value * 0.001))|0;
+                    _.samples = _.waitSamples;
+                    _.isEnded = false;
                 }
             },
             get: function() {
@@ -47,6 +60,17 @@
             }
         }
     });
+
+    $.bang = function() {
+        var _ = this._;
+        if (_.type === TYPE_TIMEOUT) {
+            _.samples     = _.waitSamples;
+            _.currentTime = 0;
+            _.isEnded     = false;
+        }
+        _.emit("bang");
+        return this;
+    };
     
     $.seq = function(seq_id) {
         var cell = this.cell;
@@ -55,33 +79,40 @@
         if (this.seq_id !== seq_id) {
             this.seq_id = seq_id;
             
-            if (_.waitSamples > 0) {
-                _.waitSamples -= cell.length;
-            }
-            
-            if (_.waitSamples <= 0) {
-                var inputs = this.inputs;
-                for (var i = 0, imax = inputs.length; i < imax; ++i) {
-                    inputs[i].bang();
+            if (!_.isEnded) {
+                if (_.samples > 0) {
+                    _.samples -= cell.length;
                 }
-                timbre.fn.nextTick(ondone.bind(this));
+                
+                if (_.samples <= 0) {
+                    var inputs = this.inputs;
+                    for (var i = 0, imax = inputs.length; i < imax; ++i) {
+                        inputs[i].bang();
+                    }
+                    timbre.fn.nextTick(onended.bind(this));
+                }
+                _.currentTime += _.currentTimeIncr;
             }
-            
-            _.currentTime += _.currentTimeIncr;
         }
         return cell;
     };
     
-    var ondone = function() {
-        if (!this.isResolved) {
-            this._.waitSamples = Infinity;
-            this._.emit("done");
-            this._.deferred.resolve();
-            this.pause();
+    var onended = function() {
+        var _ = this._;
+        _.isEnded = true;
+        if (_.type === TYPE_WAIT && !this.isResolved) {
+            _.waitSamples = Infinity;
+            _.emit("ended");
+            _.deferred.resolve();
+            var stop = this.stop;
+            this.start = this.stop = timbre.fn.nop;
+            stop.call(this);
+        } else {
+            _.emit("ended");
         }
-        this.start = this.pause = timbre.fn.nop;
     };
     
     timbre.fn.register("wait", Wait);
     timbre.fn.alias("timeout", "wait");
+    
 })(timbre);
