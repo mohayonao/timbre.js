25d4086f871e4347b0ac97f49de4b8a2297317ea
mohayonao <mohayonao@gmail.com>
2012-12-23 23:25:46 +0900

    add T("pink")

---
+++ src/objects/pink.js
@@ -0,0 +1,57 @@
+(function(timbre) {
+    "use strict";
+    
+    // Voss algorithm
+    // http://www.firstpr.com.au/dsp/pink-noise/
+    
+    var MAX_KEY = 31;
+    var RANGE  = 128;
+    
+    function PinkNoise(_args) {
+        timbre.Object.call(this, _args);
+        timbre.fn.fixAR(this);
+        
+        var whites = new Uint8Array(5);
+        for (var i = 0; i < 5; ++i) {
+            whites[i] = ((Math.random() * (1<<30))|0) % 25;
+        }
+        this._.whites = whites;
+        this._.key = 0;
+    }
+    timbre.fn.extend(PinkNoise, timbre.Object);
+    
+    var $ = PinkNoise.prototype;
+    
+    $.seq = function(seq_id) {
+        var cell = this.cell;
+        var _ = this._;
+        
+        if (this.seq_id !== seq_id) {
+            this.seq_id = seq_id;
+            
+            var i, imax, j;
+            var key = _.key, whites = _.whites;
+            var mul = _.mul, add = _.add;
+            var last_key, sum, diff;            
+            
+            for (i = 0, imax = cell.length; i < imax; ++i) {
+                last_key = key++;
+                if (key > MAX_KEY) {
+                    key = 0;
+                }
+                diff = last_key ^ key;
+                for (j = sum = 0; j < 5; ++j) {
+                    if (diff & (1 << j)) {
+                        whites[j] = ((Math.random() * (1<<30))|0) % 25;
+                    }
+                    sum += whites[j];
+                }
+                cell[i] = ((sum * 0.01666666) - 1) * mul + add;
+            }
+            _.key = key;
+        }
+        return cell;
+    };
+    
+    timbre.fn.register("pink", PinkNoise);
+})(timbre);
