31a6755d4f34d54da4819e050cb30a8532e94a9a
mohayonao <mohayonao@gmail.com>
2012-12-28 13:52:23 +0900

    fix T("clip") properties

--- src/objects/clip.js
+++ src/objects/clip.js
@@ -6,21 +6,55 @@
     function ClipNode(_args) {
         timbre.Object.call(this, _args);
         
-        this._.lv = 0.8;
+        var _ = this._;
+        _.min = -0.8;
+        _.max = +0.8;
     }
     fn.extend(ClipNode);
     
     var $ = ClipNode.prototype;
     
     Object.defineProperties($, {
-        lv: {
+        minmax: {
             set: function(value) {
+                var _ = this._;
                 if (typeof value === "number") {
-                    this._.lv = Math.abs(value);
+                    _.min = -Math.abs(value);
+                    _.max = -_.min;
                 }
             },
             get: function() {
-                return this._.lv;
+                return this._.max;
+            }
+        },
+        min: {
+            set: function(value) {
+                var _ = this._;
+                if (typeof value === "number") {
+                    if (_.max < value) {
+                        _.max = value;
+                    } else {
+                        _.min = value;
+                    }
+                }
+            },
+            get: function() {
+                return this._.min;
+            }
+        },
+        max: {
+            set: function(value) {
+                var _ = this._;
+                if (typeof value === "number") {
+                    if (value < _.min) {
+                        _.min = value;
+                    } else {
+                        _.max = value;
+                    }
+                }
+            },
+            get: function() {
+                return this._.max;
             }
         }
     });
@@ -36,7 +70,7 @@
             var mul = _.mul, add = _.add;
             var i, imax = inputs.length;
             var j, jmax = cell.length;
-            var lv = _.lv;
+            var min = _.min, max = _.max;
             var tmp, x;
             
             for (j = jmax; j--; ) {
@@ -52,7 +86,7 @@
                 }
                 for (j = jmax; j--; ) {
                     x = cell[j];
-                    x = (x < -lv) ? -lv : (x > lv) ? lv : x;
+                    x = (x < min) ? min : (x > max) ? max : x;
                     cell[j] = x;
                 }
                 
@@ -66,7 +100,7 @@
                 for (i = 0; i < imax; ++i) {
                     tmp += inputs[i].process(tickID)[0];
                 }
-                tmp = (tmp < -lv) ? -lv : (tmp > lv) ? lv : tmp;
+                tmp = (tmp < min) ? min : (tmp > max) ? max : tmp;
                 tmp = tmp * mul + add;
                 for (j = jmax; j--; ) {
                     cell[j] = tmp;
