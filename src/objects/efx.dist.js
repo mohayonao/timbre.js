77066534f20a3b7a088620bcaa75d0403f5c013d
mohayonao <mohayonao@gmail.com>
2012-12-31 15:33:47 +0900

    update T("efx.dist").cutoff

--- src/objects/efx.dist.js
+++ src/objects/efx.dist.js
@@ -6,6 +6,12 @@
     function EfxDistNode(_args) {
         timbre.Object.call(this, _args);
         fn.fixAR(this);
+
+        var _ = this._;
+        _.samplerate = timbre.samplerate;
+        _.x1 = _.x2 = _.y1 = _.y2 = 0;
+        _.b0 = _.b1 = _.b2 = _.a1 = _.a2 = 0;
+        _.cutoff = 0;
         
         this.once("init", oninit);
     }
@@ -37,6 +43,16 @@
             },
             get: function() {
                 return this._.postGain;
+            }
+        },
+        cutoff: {
+            set: function(value) {
+                if (typeof value === "number" && value > 0) {
+                    this._.cutoff = value;
+                }
+            },
+            get: function() {
+                return this._.cutoff;
             }
         }
     });
@@ -71,16 +87,89 @@
             var preScale = _.preScale;
             var limit    = _.limit;
             var mul = _.mul, add = _.add;
-            var x;
+            var i, imax;
+            var x0, y0;
             
-            for (var i = cell.length; i--; ) {
-                x = cell[i] * preScale;
-                x = (x > limit) ? limit : (x < -limit) ? -limit : x;
-                cell[i] = x * mul + add;
+            if (_.cutoff) {
+                if (_.prevCutoff !== _.cutoff) {
+                    _.prevCutoff = _.cutoff;
+                    lowpass_params(_);
+                }
+                
+                var x1 = _.x1;
+                var x2 = _.x2;
+                var y1 = _.y1;
+                var y2 = _.y2;
+                
+                var b0 = _.b0;
+                var b1 = _.b1;
+                var b2 = _.b2;
+                var a1 = _.a1;
+                var a2 = _.a2;
+                
+                for (i = 0, imax = cell.length; i < imax; ++i) {
+                    x0 = cell[i] * preScale;
+                    y0 = b0 * x0 + b1 * x1 + b2 * x2 - a1 * y1 - a2 * y2;
+                    
+                    y0 = (y0 > limit) ? limit : (y0 < -limit) ? -limit : y0;
+                    
+                    cell[i] = y0 * mul + add;
+                    
+                    x2 = x1;
+                    x1 = x0;
+                    y2 = y1;
+                    y1 = y0;
+                }
+                
+                // flushDenormalFloatToZero
+                if ((x1 > 0 && x1 <  1e-4) || (x1 < 0 && x1 > -1e-4)) {
+                    x1 = 0;
+                }
+                if ((y1 > 0 && y1 <  1e-4) || (y1 < 0 && y1 > -1e-4)) {
+                    y1 = 0;
+                }
+                
+                _.x1 = x1;
+                _.x2 = x2;
+                _.y1 = y1;
+                _.y2 = y2;
+            } else {
+                for (i = cell.length; i--; ) {
+                    x0 = cell[i] * preScale;
+                    x0 = (x0 > limit) ? limit : (x0 < -limit) ? -limit : x0;
+                    cell[i] = x0 * mul + add;
+                }
             }
         }
         
         return cell;
+    };
+
+    var lowpass_params = function(_) {
+        var cutoff = _.cutoff / (_.samplerate * 0.5);
+        
+        if (cutoff >= 1) {
+            _.b0 = 1;
+            _.b1 = _.b2 = _.a1 = _.a2 = 0;
+        } else if (cutoff <= 0) {
+            _.b0 = _.b1 = _.b2 = _.a1 = _.a2 = 0;
+        } else {
+            var resonance = 1;
+            var g = Math.pow(10.0, 0.05 * resonance);
+            var d = Math.sqrt((4 - Math.sqrt(16 - 16 / (g * g))) * 0.5);
+            
+            var theta = Math.PI * cutoff;
+            var sn = 0.5 * d * Math.sin(theta);
+            var beta = 0.5 * (1 - sn) / (1 + sn);
+            var gamma = (0.5 + beta) * Math.cos(theta);
+            var alpha = 0.25 * (0.5 + beta - gamma);
+            
+            _.b0 = 2 * alpha;
+            _.b1 = 4 * alpha;
+            _.b2 = _.b0;
+            _.a1 = 2 * -gamma;
+            _.a2 = 2 * beta;
+        }
     };
     
     fn.register("efx.dist", EfxDistNode);
