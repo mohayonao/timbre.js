281d5cbb08e24484eadca7dbe20061842eaf57d6
mohayonao <mohayonao@gmail.com>
2013-01-01 16:33:05 +0900

    add T("pluck")

---
+++ docs.md/R-pluck.md
@@ -0,0 +1,21 @@
+T("pluck")
+==========
+{ar} Karplus–Strong string synthesis
+
+## Description ##
+Karplus Strong方式による撥弦音
+
+```timbre
+T("pluck", {freq:500, mul:0.5}).bang().play();
+```
+
+## Properties ##
+- `freq` _(Number)_
+  - 周波数をセットする. デフォルト値は **440**
+
+## Methods ##
+- `bang()`
+  - 撥弦を開始します
+  
+## Source ##
+https://github.com/mohayonao/timbre.js/blob/master/src/objects/pluck.js
---
+++ src/objects/pluck.js
@@ -0,0 +1,81 @@
+(function(timbre) {
+    "use strict";
+    
+    var fn = timbre.fn;
+    
+    function PluckNode(_args) {
+        timbre.Object.call(this, _args);
+        
+        this._.freq   = 440;
+        this._.buffer = null;
+        this._.readIndex  = 0;
+        this._.writeIndex = 0;
+    }
+    fn.extend(PluckNode);
+    
+    var $ = PluckNode.prototype;
+    
+    Object.defineProperties($, {
+        freq: {
+            set: function(value) {
+                if (typeof value === "number" & value >= 55) {
+                    this._.freq = value;
+                }
+            },
+            get: function() {
+                return this._.freq;
+            }
+        }
+    });
+    
+    $.bang = function() {
+        var _ = this._;
+        var size   = (timbre.samplerate / _.freq + 0.5)|0;
+        var buffer = _.buffer = new Float32Array(size << 1);
+        for (var i = size; i--; ) {
+            buffer[i] = Math.random() * 2 - 1;
+        }
+        _.readIndex  = 0;
+        _.writeIndex = size;
+        _.emit("bang");
+        return this;
+    };
+    
+    $.process = function(tickID) {
+        var cell = this.cell;
+        var _ = this._;
+        
+        if (this.tickID !== tickID) {
+            this.tickID = tickID;
+            
+            var buffer = _.buffer;
+            if (buffer) {
+                var bufferLength = buffer.length;
+                var readIndex  = _.readIndex;
+                var writeIndex = _.writeIndex;
+                var mul = _.mul, add = _.add;
+                var x, i, imax = cell.length;
+                
+                for (i = 0; i < imax; ++i) {
+                    x = buffer[readIndex++];
+                    if (readIndex >= bufferLength) {
+                        readIndex = 0;
+                    }
+                    x = (x + buffer[readIndex]) * 0.5;
+                    buffer[writeIndex++] = x;
+                    if (writeIndex >= bufferLength) {
+                        writeIndex = 0;
+                    }
+                    cell[i] = x * mul + add;
+                }
+                _.readIndex  = readIndex;
+                _.writeIndex = writeIndex;
+            }
+        }
+        
+        return cell;
+    };
+    
+    fn.register("pluck", PluckNode);
+    
+})(timbre);
