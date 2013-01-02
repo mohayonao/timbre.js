0110fd3958bac4540c60ebeb3d26b03022947537
mohayonao <mohayonao@gmail.com>
2013-01-02 15:07:42 +0900

    update T("SynthDef")

--- src/objects/synthdef.js
+++ src/objects/synthdef.js
@@ -9,7 +9,7 @@
 
         var _ = this._;
 
-        _.poly     = 4;        
+        _.poly     = 4;
         _.genList  = [];
         _.genDict  = {};
         _.synthdef = null;
@@ -22,6 +22,16 @@
     var $ = SynthDefNode.prototype;
     
     Object.defineProperties($, {
+        def: {
+            set: function(value) {
+                if (typeof value === "function") {
+                    this._.synthdef = value;
+                }
+            },
+            get: function() {
+                return this._.synthdef;
+            }
+        },
         poly: {
             set: function(value) {
                 if (typeof value === "number") {
@@ -33,32 +43,23 @@
             get: function() {
                 return this._.poly;
             }
-        },
-        env: {
-            set: function(value) {
-                if (typeof value === "object" && value.constructor === Object) {
-                    if (typeof value.type === "string") {
-                        this._.env = value;
-                    }
-                }
-            },
-            get: function() {
-                return this._.env;
-            }
-        },
-        def: {
-            set: function(value) {
-                if (typeof value === "function") {
-                    this._.synthdef = value;
-                }
-            },
-            get: function() {
-                return this._.synthdef;
-            }
         }
     });
     
-    var noteOn = function(freq, noteNum, velocity) {
+    var doneAction = function(opts) {
+        remGen.call(this, opts.gen);
+    };
+    
+    var remGen = function(gen) {
+        var _ = this._;
+        var i = _.genList.indexOf(gen);
+        if (i !== -1) {
+            _.genList.splice(i, 1);
+        }
+        _.genDict[gen.noteNum] = null;
+    };
+    
+    var noteOn = function(noteNum, freq, velocity, _opts) {
         velocity |= 0;
         if (velocity <= 0) {
             this.noteOff(this, noteNum);
@@ -72,12 +73,24 @@
             _.remGen(gen);
         }
         
-        gen = this._.synthdef.call(this, {freq:freq, noteNum:noteNum, velocity:velocity});
+        var opts = {
+            freq    : freq,
+            noteNum : noteNum,
+            velocity: velocity
+        };
+        if (_opts) {
+            for (var key in _opts) {
+                opts[key] = _opts[key];
+            }
+        }
+        opts.doneAction = doneAction.bind(this, opts);
+        
+        gen = this._.synthdef.call(this, opts);
         
         if (gen instanceof timbre.Object) {
             gen.noteNum = noteNum;
             list.push(gen);
-            dict[noteNum] = gen;
+            dict[noteNum] = opts.gen = gen;
             
             _.isEnded = false;
             
@@ -87,21 +100,6 @@
         }
     };
     
-    var remGen = function(gen) {
-        var _ = this._;
-        var i = _.genList.indexOf(gen);
-        if (i !== -1) {
-            _.genList.splice(i, 1);
-        }
-        _.genDict[gen.noteNum] = null;
-    };
-
-    $.noteOn = function(noteNum, velocity) {
-        var freq = midicps[noteNum|0] || 0;
-        noteOn.call(this, freq, noteNum, velocity);
-        return this;
-    };
-    
     var midicps = (function() {
         var table = new Float32Array(128);
         for (var i = 0; i < 128; ++i) {
@@ -109,41 +107,55 @@
         }
         return table;
     })();
-    
+
     var cpsmidi = function(cps) {
         if (cps > 0) {
-            return ((Math.log(cps * 1 / 440) * Math.LOG2E * 12 + 69) + 0.5)|0;
+            return Math.log(cps * 1 / 440) * Math.LOG2E * 12 + 69;
         } else {
             return 0;
         }
     };
     
-    $.noteOnWithFreq = function(freq, velocity) {
-        noteOn.call(this, freq, cpsmidi(freq), velocity);
+    $.noteOn = function(noteNum, velocity, _opts) {
+        var freq = midicps[noteNum] || (440 * Math.pow(2, (noteNum - 69) / 12));
+        noteOn.call(this, (noteNum + 0.5)|0, freq, velocity, _opts);
         return this;
     };
     
-    $.noteOff = function(noteNum /* , velocity */) {
+    $.noteOff = function(noteNum) {
         var gen = this._.genDict[noteNum];
-        if (gen) {
+        if (gen && gen.release) {
             gen.release();
         }
         return this;
     };
     
-    $.noteOffWithFreq = function(freq /* , velocity */) {
-        return this.noteOff(cpsmidi(freq));
+    $.noteOnWithFreq = function(freq, velocity, _opts) {
+        var noteNum = cpsmidi(freq);
+        noteOn.call(this, (noteNum + 0.5)|0, freq, velocity, _opts);
+        return this;
+    };
+    
+    $.noteOffWithFreq = function(freq) {
+        var noteNum = cpsmidi(freq);
+        return this.noteOff((noteNum + 0.5)|0);
     };
     
     $.allNoteOff = function() {
+        var list = this._.genList;
+        for (var i = 0, imax = list.length; i < imax; ++i) {
+            if (list[i].release) {
+                list[i].release();
+            }
+        }
+    };
+    
+    $.allSoundOff = function() {
         var _ = this._;
         var list = _.genList;
         var dict = _.genDict;
-        var gen;
         while (list.length) {
-            gen = list.shift();
-            gen.release();
-            delete dict[gen.noteNum];
+            delete dict[list.shift().noteNum];
         }
     };
     
@@ -171,7 +183,7 @@
                     }
                 }
                 if (imax === 0) {
-                    fn.nextTick(__onended.bind(this));
+                    fn.nextTick(fn.onended.bind(null, this));
                 }
             }
             
@@ -183,59 +195,87 @@
     
     fn.register("SynthDef", SynthDefNode);
     
-    fn.register("OscGen", function(_args) {
-        var instance = new SynthDefNode(_args);
-        
-        instance._.wave = "sin";
-        
-        Object.defineProperties(instance, {
-            wave: {
-                set: function(value) {
-                    if (typeof value === "string") {
-                        this._.wave = value;
-                    }
-                },
-                get: function() {
-                    return this._.wave;
+    
+    var env_desc = {
+        set: function(value) {
+            if (typeof value === "object" && value.constructor === Object) {
+                if (typeof value.type === "string") {
+                    this._.env = value;
                 }
             }
-        });
+        },
+        get: function() {
+            return this._.env;
+        }
+    };
+    
+    fn.register("OscGen", (function() {
+
+        var wave_desc = {
+            set: function(value) {
+                if (typeof value === "string") {
+                    this._.wave = value;
+                }
+            },
+            get: function() {
+                return this._.wave;
+            }
+        };
         
         var synthdef = function(opts) {
-            var _ = this._, synth;
+            var _ = this._;
+            var synth, env, envtype;
+            
+            env = _.env || {};
+            envtype = env.type || "perc";
             
             synth = timbre("osc", {wave:_.wave, freq:opts.freq, mul:opts.velocity/128});
-            synth = timbre(_.env.type, _.env, synth);
-            synth.on("ended", function() {
-                _.remGen(this);
-            }).bang();
+            synth = timbre(envtype, env, synth).on("ended", opts.doneAction).bang();
             
             return synth;
         };
         
-        instance.def = synthdef;
-        
-        return instance;
-    });
-
-    fn.register("PluckGen", function(_args) {
-        var instance = new SynthDefNode(_args);
+        return function(_args) {
+            var instance = new SynthDefNode(_args);
+            
+            instance._.wave = "sin";
+            
+            Object.defineProperties(instance, {
+                env: env_desc, wave: wave_desc
+            });
+            
+            instance.def = synthdef;
+            
+            return instance;
+        };
+    })());
+    
+    fn.register("PluckGen", (function() {
         
         var synthdef = function(opts) {
-            var _ = this._, synth;
+            var _ = this._;
+            var synth, env, envtype;
+            
+            env = _.env || {};
+            envtype = env.type || "perc";
             
             synth = timbre("pluck", {freq:opts.freq, mul:opts.velocity/128}).bang();
-            synth = timbre(_.env.type, _.env, synth);
-            synth.on("ended", function() {
-                _.remGen(this);
-            }).bang();
+            synth = timbre(envtype, env, synth).on("ended", opts.doneAction).bang();
             
             return synth;
         };
         
-        instance.def = synthdef;
-        
-        return instance;
-    });
+        return function(_args) {
+            var instance = new SynthDefNode(_args);
+            
+            Object.defineProperties(instance, {
+                env: env_desc
+            });
+            
+            instance.def = synthdef;
+            
+            return instance;
+        };
+    })());
     
 })(timbre);
