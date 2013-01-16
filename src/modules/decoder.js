e908865889c3dda9b4b88180c6250426093ab399
mohayonao <mohayonao@gmail.com>
2013-01-16 17:46:46 +0900

    fix decoder

---
+++ src/modules/decoder.js
@@ -0,0 +1,321 @@
+(function() {
+    "use strict";
+    
+    function Decoder() {}
+    
+    Decoder.prototype.decode = function(type, data, onloadedmetadata, onloadeddata) {
+        if (timbre.envtype === "browser") {
+            if (type === "wav") {
+                return wav_decoder(data, onloadedmetadata, onloadeddata);
+            } else if (webkit_decoder) {
+                return webkit_decoder(data, onloadedmetadata, onloadeddata);
+            } else if (moz_decoder) {
+                return moz_decoder(data, onloadedmetadata, onloadeddata);
+            }
+        } else if (timbre.envtype === "node") {
+            if (type === "wav") {
+                return wav_decoder(data, onloadedmetadata, onloadeddata);
+            } else if (type === "ogg") {
+                return node_ogg_decoder(data, onloadedmetadata, onloadeddata);
+            } else if (type === "mp3") {
+                return node_mp3_decoder(data, onloadedmetadata, onloadeddata);
+            }
+        }
+        onloadedmetadata(false);
+    };
+    timbre.modules.Decoder = Decoder;
+
+    var getBinaryWithPath;
+    if (timbre.envtype === "browser") {
+        getBinaryWithPath = function(path, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", path, true);
+            xhr.responseType = "arraybuffer";
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    callback(new Uint8Array(xhr.response));
+                } else {
+                    var msg = xhr.status + " " + xhr.statusText;
+                    callback(msg);
+                }
+            };
+            xhr.send();
+        };
+    } else {
+        getBinaryWithPath = function(path, callback) {
+            var fs = require("fs");
+            fs.readFile(path, function(err, data) {
+                if (!err) {
+                    callback(new Uint8Array(data));
+                } else {
+                    var msg = "can't read file";
+                    callback(msg);
+                }
+            });
+        };
+    }
+    
+    var deinterleave = function(list) {
+        var result = new list.constructor(list.length>>1);
+        var i = list.length, j = result.length;
+        if (i % 2) {
+            i -= 1;
+            j |= 0;
+        }
+        while (j) {
+            result[--j] = (list[--i] + list[--i]) * 0.5;
+        }
+        return result;
+    };
+    
+    var _24bit_to_32bit = function(uint8) {
+        var b0, b1, b2, bb, x;
+        var int32 = new Int32Array(uint8.length / 3);
+        for (var i = 0, imax = uint8.length, j = 0; i < imax; ) {
+            b0 = uint8[i++] ,b1 = uint8[i++], b2 = uint8[i++];
+            bb = b0 + (b1 << 8) + (b2 << 16);
+            x = (bb & 0x800000) ? -((bb^0xFFFFFF)+1) : bb;
+            int32[j++] = x;
+        }
+        return int32;
+    };
+    
+    var wav_decoder = function(filepath, onloadedmetadata, onloadeddata) {
+        getBinaryWithPath(filepath, function(data) {
+            if (data[0] !== 0x52 || data[1] !== 0x49 ||
+                data[2] !== 0x46 || data[3] !== 0x46) { // 'RIFF'
+                    // "HeaderError: not exists 'RIFF'"
+                    return onloadedmetadata(false);
+            }
+            
+            var l1 = data[4] + (data[5]<<8) + (data[6]<<16) + (data[7]<<24);
+            if (l1 + 8 !== data.length) {
+                // "HeaderError: invalid data size"
+                return onloadedmetadata(false);
+            }
+            
+            if (data[ 8] !== 0x57 || data[ 9] !== 0x41 ||
+                data[10] !== 0x56 || data[11] !== 0x45) { // 'WAVE'
+                    // "HeaderError: not exists 'WAVE'"
+                    return onloadedmetadata(false);
+            }
+            
+            if (data[12] !== 0x66 || data[13] !== 0x6D ||
+                data[14] !== 0x74 || data[15] !== 0x20) { // 'fmt '
+                    // "HeaderError: not exists 'fmt '"
+                    return onloadedmetadata(false);
+            }
+            
+            // var byteLength = data[16] + (data[17]<<8) + (data[18]<<16) + (data[19]<<24);
+            // var linearPCM  = data[20] + (data[21]<<8);
+            var channels   = data[22] + (data[23]<<8);
+            var samplerate = data[24] + (data[25]<<8) + (data[26]<<16) + (data[27]<<24);
+            // var dataSpeed  = data[28] + (data[29]<<8) + (data[30]<<16) + (data[31]<<24);
+            // var blockSize  = data[32] + (data[33]<<8);
+            var bitSize    = data[34] + (data[35]<<8);
+            
+            if (data[36] !== 0x64 || data[37] !== 0x61 ||
+                data[38] !== 0x74 || data[39] !== 0x61) { // 'data'
+                    // "HeaderError: not exists 'data'"
+                    return onloadedmetadata(false);
+            }
+            
+            var l2 = data[40] + (data[41]<<8) + (data[42]<<16) + (data[43]<<24);
+            var duration = ((l2 / channels) >> 1) / samplerate;
+
+            if (l2 > data.length - 44) {
+                // "HeaderError: not exists data"
+                return onloadedmetadata(false);
+            }
+            
+            var buffer = new Float32Array((duration * samplerate)|0);
+            
+            onloadedmetadata({
+                samplerate: samplerate,
+                buffer    : buffer,
+                duration  : duration
+            });
+            
+            if (bitSize === 8) {
+                data = new Int8Array(data.buffer, 44);
+            } else if (bitSize === 16) {
+                data = new Int16Array(data.buffer, 44);
+            } else if (bitSize === 32) {
+                data = new Int32Array(data.buffer, 44);
+            } else if (bitSize === 24) {
+                data = _24bit_to_32bit(new Uint8Array(data.buffer, 44));
+            }
+            
+            if (channels === 2) {
+                data = deinterleave(data);
+            }
+            
+            var k = 1 / ((1 << (bitSize-1)) - 1);
+            for (var i = buffer.length; i--; ) {
+                buffer[i] = data[i] * k;
+            }
+            
+            onloadeddata();
+        });
+    };
+    
+    
+    var webkit_decoder = (function() {
+        
+        if (typeof webkitAudioContext !== "undefined") {
+            var ctx = new webkitAudioContext();
+            var _decode = function(data, onloadedmetadata, onloadeddata) {
+                var samplerate, duration, buffer;
+                if (typeof data === "string") {
+                    return onloadeddata(false);
+                }
+                
+                try {
+                    buffer = ctx.createBuffer(data.buffer, true);
+                } catch (e) {
+                    return onloadedmetadata(false);
+                }
+                
+                samplerate = ctx.sampleRate;
+                buffer     = buffer.getChannelData(0);
+                duration   = buffer.length / samplerate;
+                
+                onloadedmetadata({
+                    samplerate: samplerate,
+                    buffer    : buffer,
+                    duration  : duration
+                });
+                
+                onloadeddata();
+            };
+            
+            return function(filepath, onloadedmetadata, onloadeddata) {
+                if (filepath instanceof File) {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        _decode(new Uint8Array(e.target.result),
+                                onloadedmetadata, onloadeddata);
+                    };
+                    reader.readAsArrayBuffer(filepath);
+                } else {
+                    getBinaryWithPath(filepath, function(data) {
+                        _decode(data,
+                                onloadedmetadata, onloadeddata);
+                    });
+                }
+            };
+        }
+    })();
+    
+    var moz_decoder = (function() {
+        if (typeof Audio === "function" && typeof new Audio().mozSetup === "function") {
+            return function(data, onloadedmetadata, onloadeddata) {
+                var self = this;
+                var samplerate, duration, buffer;
+                var writeIndex = 0;
+                
+                var audio = new Audio(data);
+                audio.volume = 0.0;
+                audio.speed  = 2;
+                audio.addEventListener("loadedmetadata", function() {
+                    samplerate = audio.mozSampleRate;
+                    duration = audio.duration;
+                    buffer = new Float32Array((audio.duration * samplerate)|0);
+                    if (audio.mozChannels === 2) {
+                        audio.addEventListener("MozAudioAvailable", function(e) {
+                            var samples = e.frameBuffer;
+                            for (var i = 0, imax = samples.length; i < imax; i += 2) {
+                                buffer[writeIndex++] = (samples[i] + samples[i+1]) * 0.5;
+                            }
+                            self._.loadedTime = samples.length * 1000 / samplerate;
+                        }, false);
+                    } else {
+                        audio.addEventListener("MozAudioAvailable", function(e) {
+                            var samples = e.frameBuffer;
+                            for (var i = 0, imax = samples.length; i < imax; ++i) {
+                                buffer[writeIndex++] = samples[i];
+                            }
+                            self._.loadedTime = samples.length * 1000 / samplerate;
+                        }, false);
+                    }
+                    audio.play();
+                    setTimeout(function() {
+                        onloadedmetadata({
+                            samplerate: samplerate,
+                            buffer    : buffer,
+                            duration  : duration
+                        });
+                    }, 1000);
+                }, false);
+                audio.addEventListener("ended", function() {
+                    onloadeddata();
+                }, false);
+                audio.addEventListener("error", function() {
+                    self._.emit("error");
+                }, false);
+                audio.load();
+            };
+        }
+    })();
+    
+    var node_ogg_decoder = function(filepath, onloadedmetadata/*, onloadeddata*/) {
+        onloadedmetadata(false);
+    };
+    
+    var node_mp3_decoder = function(filepath, onloadedmetadata, onloadeddata) {
+        var fs   = require("fs");
+        var lame = require("lame");
+        var decoder = new lame.Decoder();
+        var bytes = [];
+        var samplerate, duration, buffer;
+        var channels, bitDepth;
+        
+        decoder.on("format", function(format) {
+            // console.log("format", format);
+            samplerate = format.sampleRate;
+            channels   = format.channels;
+            bitDepth   = format.bitDepth;
+        });
+        decoder.on("data", function(data) {
+            for (var i = 0, imax = data.length; i < imax; ++i) {
+                bytes.push(data[i]);
+            }
+        });
+        decoder.on("end", function() {
+            var length = bytes.length / channels / (bitDepth / 8);
+            
+            duration = length / samplerate;
+            buffer = new Float32Array(length);
+            
+            var uint8 = new Uint8Array(bytes);
+            var data;
+            if (bitDepth === 16) {
+                data = new Int16Array(uint8.buffer);
+            } else if (bitDepth === 8) {
+                data = new Int8Array(uint8.buffer);
+            } else if (bitDepth === 24) {
+                data = _24bit_to_32bit(uint8.buffer);
+            }
+            
+            if (channels === 2) {
+                data = deinterleave(data);
+            }
+            
+            var k = 1 / ((1 << (bitDepth-1)) - 1);
+            for (var i = buffer.length; i--; ) {
+                buffer[i] = data[i] * k;
+            }
+            
+            onloadedmetadata({
+                samplerate: samplerate,
+                buffer    : buffer,
+                duration  : duration
+            });
+
+            
+            onloadeddata();
+        });
+        fs.createReadStream(filepath).pipe(decoder);
+    };
+    
+})();
--- src/objects/audio.js
+++ src/objects/audio.js
@@ -45,7 +45,7 @@
     
     function getLoadFunctionForBrowser() {
         return function() {
-            var self = this, _ = this._;
+            var _ = this._;
             var dfd = new modules.Deferred();
             
             var args = arguments, i = 0;
@@ -76,7 +76,6 @@
             
             if (typeof src === "string") {
                 if (src !== "") {
-                    var noUseByteData = false;
                     if (/.*\.wav/.test(src)) {
                         decoderList = [wav_decoder];
                     } else {
@@ -84,41 +83,17 @@
                             decoderList = [webkit_decoder];
                         } else if (moz_decoder) {
                             decoderList = [moz_decoder];
-                            noUseByteData = true;
                         }
                     }
-                    
-                    if (noUseByteData) {
-                        then.call(this, decoderList, src, dfd);
-                        this._.emit("load");
-                    } else {
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("GET", src, true);
-                        xhr.responseType = "arraybuffer";
-                        xhr.onload = function() {
-                            if (xhr.status === 200) {
-                                then.call(self, decoderList,
-                                          new Uint8Array(xhr.response), dfd);
-                            } else {
-                                var msg = xhr.status + " " + xhr.statusText;
-                                self._.emit("error", msg);
-                                dfd.reject();
-                            }
-                        };
-                        xhr.send();
-                        this._.emit("load");
-                    }
+                    then.call(this, decoderList, src, dfd);
+                    this._.emit("load");
                 } else {
                     dfd.reject();
                 }
-            } else if (src instanceof File) {
+            } else {
                 if (webkit_decoder) {
-                    var reader = new FileReader();
-                    reader.onload = function(e) {
-                        then.call(self, [webkit_decoder],
-                                  new Uint8Array(e.target.result), dfd);
-                    };
-                    reader.readAsArrayBuffer(src);
+                    decoderList = [webkit_decoder];
+                    then.call(this, decoderList, src, dfd);
                     this._.emit("load");
                 } else {
                     var msg = "no support";
@@ -168,22 +143,8 @@
                         then.call(self, [node_ogg_decoder], src, dfd);
                     } else if (/.*\.mp3/.test(src)) {
                         then.call(self, [node_mp3_decoder], src, dfd);
-                    } else {
-                        fs.readFile(src, function(err, data) {
-                            if (err) {
-                                var msg = "can't read file";
-                                self._.emit("error", msg);
-                                return dfd.reject();
-                            }
-                            var decoderList;
-                            if (typeof src === "string") {
-                                if (/.*\.wav/.test(src)) {
-                                    decoderList = [wav_decoder];
-                                }
-                            }
-                            then.call(self, decoderList,
-                                      new Uint8Array(data), dfd);
-                        });
+                    } else if (/.*\.wav/.test(src)) {
+                        then.call(self, [wav_decoder], src, dfd);
                     }
                 });
                 this._.emit("load");
@@ -191,34 +152,6 @@
             return dfd.promise();
         };
     }
-    
-    
-    
-    var deinterleave = function(list) {
-        var result = new list.constructor(list.length>>1);
-        var i = list.length, j = result.length;
-        if (i % 2) {
-            i -= 1;
-            j |= 0;
-        }
-        while (j) {
-            result[--j] = (list[--i] + list[--i]) * 0.5;
-        }
-        return result;
-    };
-    
-    var _24bit_to_32bit = function(uint8) {
-        var b0, b1, b2, bb, x;
-        var int32 = new Int32Array(uint8.length / 3);
-        for (var i = 0, imax = uint8.length, j = 0; i < imax; ) {
-            b0 = uint8[i++] ,b1 = uint8[i++], b2 = uint8[i++];
-            bb = b0 + (b1 << 8) + (b2 << 16);
-            x = (bb & 0x800000) ? -((bb^0xFFFFFF)+1) : bb;
-            int32[j++] = x;
-        }
-        return int32;
-    };
-    
     
     var then = function(decoderList, data, dfd) {
         var self = this;
@@ -250,6 +183,8 @@
         };
         
         var onloadeddata = function() {
+            self._.isLoaded  = true;
+            self._.plotFlush = true;
             self._.emit("loadeddata");
             dfd.resolveWith(self);
         };
@@ -258,7 +193,7 @@
             if (decoderList.length > 0) {
                 var decoder = decoderList.shift();
                 if (decoder) {
-                    decoder.call(self, data, onloadedmetadata, onloadeddata);
+                    decoder(data, onloadedmetadata, onloadeddata);
                 } else {
                     iter();
                 }
@@ -272,29 +207,9 @@
     
     var webkit_decoder = (function() {
         if (typeof webkitAudioContext !== "undefined") {
-            var ctx = new webkitAudioContext();
             return function(data, onloadedmetadata, onloadeddata) {
-                var samplerate, duration, buffer;
-                try {
-                    buffer = ctx.createBuffer(data.buffer, true);
-                } catch (e) {
-                    return onloadedmetadata(false);
-                }
-                
-                samplerate = ctx.sampleRate;
-                buffer     = buffer.getChannelData(0);
-                duration   = buffer.length / samplerate;
-                
-                onloadedmetadata({
-                    samplerate: samplerate,
-                    buffer    : buffer,
-                    duration  : duration
-                });
-                
-                this._.isLoaded  = true;
-                this._.plotFlush = true;
-                
-                onloadeddata();
+                var decoder = new modules.Decoder();
+                decoder.decode("webkit", data, onloadedmetadata, onloadeddata);
             };
         }
     })();
@@ -302,196 +217,24 @@
     var moz_decoder = (function() {
         if (typeof Audio === "function" && typeof new Audio().mozSetup === "function") {
             return function(data, onloadedmetadata, onloadeddata) {
-                var self = this;
-                var samplerate, duration, buffer;
-                var writeIndex = 0;
-                
-                var audio = new Audio(data);
-                audio.volume = 0.0;
-                audio.speed  = 2;
-                audio.addEventListener("loadedmetadata", function() {
-                    samplerate = audio.mozSampleRate;
-                    duration = audio.duration;
-                    buffer = new Float32Array((audio.duration * samplerate)|0);
-                    if (audio.mozChannels === 2) {
-                        audio.addEventListener("MozAudioAvailable", function(e) {
-                            var samples = e.frameBuffer;
-                            for (var i = 0, imax = samples.length; i < imax; i += 2) {
-                                buffer[writeIndex++] = (samples[i] + samples[i+1]) * 0.5;
-                            }
-                            self._.loadedTime = samples.length * 1000 / samplerate;
-                        }, false);
-                    } else {
-                        audio.addEventListener("MozAudioAvailable", function(e) {
-                            var samples = e.frameBuffer;
-                            for (var i = 0, imax = samples.length; i < imax; ++i) {
-                                buffer[writeIndex++] = samples[i];
-                            }
-                            self._.loadedTime = samples.length * 1000 / samplerate;
-                        }, false);
-                    }
-                    audio.play();
-                    setTimeout(function() {
-                        onloadedmetadata({
-                            samplerate: samplerate,
-                            buffer    : buffer,
-                            duration  : duration
-                        });
-                    }, 1000);
-                }, false);
-                audio.addEventListener("ended", function() {
-                    self._.isLoaded  = true;
-                    self._.plotFlush = true;
-                    onloadeddata();
-                }, false);
-                audio.addEventListener("error", function() {
-                    self._.emit("error");
-                }, false);
-                audio.load();
+                var decoder = new modules.Decoder();
+                decoder.decode("moz", data, onloadedmetadata, onloadeddata);
             };
         }
     })();
     
     var wav_decoder = function(data, onloadedmetadata, onloadeddata) {
-        if (data[0] !== 0x52 || data[1] !== 0x49 ||
-            data[2] !== 0x46 || data[3] !== 0x46) { // 'RIFF'
-            // "HeaderError: not exists 'RIFF'"
-            return onloadedmetadata(false);
-        }
-        
-        var l1 = data[4] + (data[5]<<8) + (data[6]<<16) + (data[7]<<24);
-        if (l1 + 8 !== data.length) {
-            // "HeaderError: invalid data size"
-            return onloadedmetadata(false);
-        }
-        
-        if (data[ 8] !== 0x57 || data[ 9] !== 0x41 ||
-            data[10] !== 0x56 || data[11] !== 0x45) { // 'WAVE'
-            // "HeaderError: not exists 'WAVE'"
-            return onloadedmetadata(false);
-        }
-        
-        if (data[12] !== 0x66 || data[13] !== 0x6D ||
-            data[14] !== 0x74 || data[15] !== 0x20) { // 'fmt '
-            // "HeaderError: not exists 'fmt '"
-            return onloadedmetadata(false);
-        }
-        
-        // var byteLength = data[16] + (data[17]<<8) + (data[18]<<16) + (data[19]<<24);
-        // var linearPCM  = data[20] + (data[21]<<8);
-        var channels   = data[22] + (data[23]<<8);
-        var samplerate = data[24] + (data[25]<<8) + (data[26]<<16) + (data[27]<<24);
-        // var dataSpeed  = data[28] + (data[29]<<8) + (data[30]<<16) + (data[31]<<24);
-        // var blockSize  = data[32] + (data[33]<<8);
-        var bitSize    = data[34] + (data[35]<<8);
-        
-        if (data[36] !== 0x64 || data[37] !== 0x61 ||
-            data[38] !== 0x74 || data[39] !== 0x61) { // 'data'
-            // "HeaderError: not exists 'data'"
-            return onloadedmetadata(false);
-        }
-        
-        var l2 = data[40] + (data[41]<<8) + (data[42]<<16) + (data[43]<<24);
-        var duration = ((l2 / channels) >> 1) / samplerate;
-
-        if (l2 > data.length - 44) {
-            // "HeaderError: not exists data"
-            return onloadedmetadata(false);
-        }
-        
-        var buffer = new Float32Array((duration * samplerate)|0);
-        
-        onloadedmetadata({
-            samplerate: samplerate,
-            buffer    : buffer,
-            duration  : duration
-        });
-        
-        if (bitSize === 8) {
-            data = new Int8Array(data.buffer, 44);
-        } else if (bitSize === 16) {
-            data = new Int16Array(data.buffer, 44);
-        } else if (bitSize === 32) {
-            data = new Int32Array(data.buffer, 44);
-        } else if (bitSize === 24) {
-            data = _24bit_to_32bit(new Uint8Array(data.buffer, 44));
-        }
-        
-        if (channels === 2) {
-            data = deinterleave(data);
-        }
-        
-        var k = 1 / ((1 << (bitSize-1)) - 1);
-        for (var i = buffer.length; i--; ) {
-            buffer[i] = data[i] * k;
-        }
-        
-        this._.isLoaded  = true;
-        this._.plotFlush = true;
-        
-        onloadeddata();
+        var decoder = new modules.Decoder();
+        decoder.decode("wav", data, onloadedmetadata, onloadeddata);
     };
     
-    var node_ogg_decoder = function(filepath, onloadedmetadata) {
-        onloadedmetadata(false);
+    var node_ogg_decoder = function(filepath, onloadedmetadata, onloadeddata) {
+        var decoder = new modules.Decoder();
+        decoder.decode("ogg", filepath, onloadedmetadata, onloadeddata);
     };
     
     var node_mp3_decoder = function(filepath, onloadedmetadata, onloadeddata) {
-        var fs   = require("fs");
-        var lame = require("lame");
-        var self = this;
-        var decoder = new lame.Decoder();
-        var bytes = [];
-        var samplerate, duration, buffer;
-        var channels, bitDepth;
-        
-        decoder.on("format", function(format) {
-            // console.log("format", format);
-            samplerate = format.sampleRate;
-            channels   = format.channels;
-            bitDepth   = format.bitDepth;
-        });
-        decoder.on("data", function(data) {
-            for (var i = 0, imax = data.length; i < imax; ++i) {
-                bytes.push(data[i]);
-            }
-        });
-        decoder.on("end", function() {
-            var length = bytes.length / channels / (bitDepth / 8);
-            
-            duration = length / samplerate;
-            buffer = new Float32Array(length);
-            
-            var uint8 = new Uint8Array(bytes);
-            var data;
-            if (bitDepth === 16) {
-                data = new Int16Array(uint8.buffer);
-            } else if (bitDepth === 8) {
-                data = new Int8Array(uint8.buffer);
-            } else if (bitDepth === 24) {
-                data = _24bit_to_32bit(uint8.buffer);
-            }
-            
-            if (channels === 2) {
-                data = deinterleave(data);
-            }
-            
-            var k = 1 / ((1 << (bitDepth-1)) - 1);
-            for (var i = buffer.length; i--; ) {
-                buffer[i] = data[i] * k;
-            }
-            
-            onloadedmetadata({
-                samplerate: samplerate,
-                buffer    : buffer,
-                duration  : duration
-            });
-
-            self._.isLoaded  = true;
-            self._.plotFlush = true;
-            
-            onloadeddata();
-        });
-        fs.createReadStream(filepath).pipe(decoder);
+        var decoder = new modules.Decoder();
+        decoder.decode("mp3", filepath, onloadedmetadata, onloadeddata);
     };
 })();
