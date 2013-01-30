c6735031f204002b7f3227dc4c5144f0f5a12315
mohayonao <mohayonao@gmail.com>
2013-01-30 19:22:34 +0900

    add T("reverb")

---
+++ src/objects/reverb.js
@@ -0,0 +1,74 @@
+(function(T) {
+    "use strict";
+    
+    var fn = T.fn;
+    var Reverb = T.modules.Reverb;
+    
+    function ReverbNode(_args) {
+        T.Object.call(this, _args);
+        fn.fixAR(this);
+        
+        this._.reverb = new Reverb(T.samplerate, T.cellsize);
+    }
+    fn.extend(ReverbNode);
+    
+    var $ = ReverbNode.prototype;
+    
+    Object.defineProperties($, {
+        room: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    value = (value > 1) ? 1 : (value < 0) ? 0 : value;
+                    this._.reverb.setRoomSize(value);
+                }
+            },
+            get: function() {
+                return this._.reverb.roomsize;
+            }
+        },
+        damp: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    value = (value > 1) ? 1 : (value < 0) ? 0 : value;
+                    this._.reverb.setDamp(value);
+                }
+            },
+            get: function() {
+                return this._.reverb.damp;
+            }
+        },
+        mix: {
+            set: function(value) {
+                if (typeof value === "number") {
+                    value = (value > 1) ? 1 : (value < 0) ? 0 : value;
+                    this._.reverb.wet = value;
+                }
+            },
+            get: function() {
+                return this._.reverb.wet;
+            }
+        }
+    });
+    
+    $.process = function(tickID) {
+        var _ = this._;
+        var cell = this.cell;
+        
+        if (this.tickID !== tickID) {
+            this.tickID = tickID;
+            
+            fn.inputSignalAR(this);
+            
+            if (!_.bypassed) {
+                _.reverb.process(cell);
+            }
+            
+            fn.outputSignalAR(this);
+        }
+        
+        return cell;
+    };
+    
+    fn.register("reverb", ReverbNode);
+    
+})(timbre);
