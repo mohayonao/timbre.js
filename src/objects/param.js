0996d9c30968ff4f36b09b5c1c569d167da2278f
mohayonao <mohayonao@gmail.com>
2013-02-10 10:25:37 +0900

    fix: counter

--- src/objects/param.js
+++ src/objects/param.js
@@ -104,16 +104,17 @@
     
     $.process = function(tickID) {
         var _ = this._;
-        var cell = this.cells[0];
         
         if (this.tickID !== tickID) {
             this.tickID = tickID;
-            
+
+            var cell = this.cells[0];
             var nodes = this.nodes;
             var i, imax = cell.length;
             var mul = _.mul, add = _.add;
             var env = _.env;
             var counter = _.counter;
+            var value;
             
             if (nodes.length) {
                 fn.inputSignalAR(this);
@@ -123,34 +124,28 @@
                 }
             }
             
-            if (counter-- > 0) {
-                if (counter <= 0) {
-                    if (_.curve === "set") {
-                        env.setNext(_.atValue, 0, Envelope.CurveTypeSet);
-                    } else {
-                        env.setNext(env.value, 0, Envelope.CurveTypeSet);
-                    }
-                    fn.nextTick(_.onended);
+            if (counter <= 0) {
+                if (_.curve === "set") {
+                    env.setNext(_.atValue, 0, Envelope.CurveTypeSet);
+                } else {
+                    env.setNext(env.value, 0, Envelope.CurveTypeSet);
                 }
-                _.counter = counter;
+                fn.nextTick(_.onended);
+                _.counter = Infinity;
             }
             
-            var value, emit = null;
             if (_.ar) {
                 for (i = 0; i < imax; ++i) {
                     value = env.next();
                     cell[i] = (cell[i] * value) * mul + add;
-                    if (emit === null) {
-                        emit = _.env.emit;
-                    }
                 }
             } else {
                 value = env.next();
                 for (i = 0; i < imax; ++i) {
                     cell[i] = (cell[i] * value) * mul + add;
                 }
-                emit = _.env.emit;
             }
+            _.counter -= cell.length;
             _.value = value;
         }
         
