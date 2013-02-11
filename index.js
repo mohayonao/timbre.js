771ed06688b71f877e1738328afcda59de339e9d
mohayonao <mohayonao@gmail.com>
2013-02-11 22:38:14 +0900

    fix: stereo decoder

--- index.js
+++ index.js
@@ -22,11 +22,10 @@ Decoder.ogg_decode = function(src, onloadedmetadata/*, onloadeddata*/) {
 Decoder.mp3_decode = function(src, onloadedmetadata, onloadeddata) {
     var decoder = new lame.Decoder();
     var bytes = [];
-    var samplerate, duration, buffer;
-    var channels, bitDepth;
+    var samplerate, channels, bufferL, bufferR, duration;
+    var bitDepth;
     
     decoder.on("format", function(format) {
-        // console.log("format", format);
         samplerate = format.sampleRate;
         channels   = format.channels;
         bitDepth   = format.bitDepth;
@@ -40,7 +39,12 @@ Decoder.mp3_decode = function(src, onloadedmetadata, onloadeddata) {
         var length = bytes.length / channels / (bitDepth / 8);
         
         duration = length / samplerate;
-        buffer = new Float32Array(length);
+        bufferL = new Float32Array(length);
+        if (channels === 2) {
+            bufferR = new Float32Array(length);
+        } else {
+            bufferR = bufferL;
+        }
         
         var uint8 = new Uint8Array(bytes);
         var data;
@@ -52,21 +56,25 @@ Decoder.mp3_decode = function(src, onloadedmetadata, onloadeddata) {
             data = _24bit_to_32bit(uint8.buffer);
         }
         
-        if (channels === 2) {
-            data = deinterleave(data);
-        }
-        
-        var k = 1 / ((1 << (bitDepth-1)) - 1);
-        for (var i = buffer.length; i--; ) {
-            buffer[i] = data[i] * k;
-        }
-        
         onloadedmetadata({
             samplerate: samplerate,
-            buffer    : buffer,
+            channels  : channels,
+            bufferL   : bufferL,
+            bufferR   : bufferR,
             duration  : duration
         });
-
+        
+        var i, imax, j, k = 1 / ((1 << (bitDepth-1)) - 1);
+        if (channels === 2) {
+            for (i = j = 0, imax = bufferL.length; i < imax; ++i) {
+                bufferL[i] = data[j++] * k;
+                bufferR[i] = data[j++] * k;
+            }
+        } else {
+            for (i = 0, imax = bufferL.length; i < imax; ++i) {
+                bufferL[i] = data[i] * k;
+            }
+        }
         
         onloadeddata();
     });
