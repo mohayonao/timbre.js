9af6e56cd69a619526074246a90c5856b82e12d9
mohayonao <mohayonao@gmail.com>
2013-02-26 12:20:49 +0900

    added: T("mono")

---
+++ docs.md/R-mono.md
@@ -0,0 +1,20 @@
+T("mono")
+=========
+{arkr} Mixdown in mono
+
+## Description ##
+###### en ######
+`T("mono")` is same as `T("+")`, but mixdown in mono.
+###### ja ######
+`T("mono")` はモノラルミックスダウンします。
+###### -- ######
+
+```timbre
+var lfo = T("sin", {freq:0.5}).kr();
+var synth = T("pan", {pos:lfo}, T("saw", {mul:0.25}));
+
+T("mono", synth).play();
+```
+
+## Source ##
+https://github.com/mohayonao/timbre.js/blob/master/src/objects/mono.js
---
+++ src/objects/mono.js
@@ -0,0 +1,27 @@
+(function(T) {
+    "use strict";
+    
+    var fn  = T.fn;
+    
+    function MonoNode(_args) {
+        T.Object.call(this, 1, _args);
+    }
+    fn.extend(MonoNode);
+    
+    MonoNode.prototype.process = function(tickID) {
+        var _ = this._;
+        if (this.tickID !== tickID) {
+            this.tickID = tickID;
+            if (_.ar) {
+                fn.inputSignalAR(this);
+                fn.outputSignalAR(this);
+            } else {
+                this.cells[0][0] = fn.inputSignalKR(this);
+                fn.outputSignalKR(this);
+            }
+        }
+        return this;
+    };
+    fn.register("mono", MonoNode);
+    
+})(timbre);
