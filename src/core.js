72d64c46cefe9ff30c38123551406151e964b59e
mohayonao <mohayonao@gmail.com>
2014-10-12 09:23:32 +0900

    fix jshint error

--- src/core.js
+++ src/core.js
@@ -2322,7 +2322,7 @@
 
     // player
     var ImplClass    = null;
-    var AudioContext = undefined;
+    var AudioContext;
     if (typeof window !== "undefined") {
       AudioContext = window.AudioContext || window.webkitAudioContext;
     }
